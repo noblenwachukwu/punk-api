6483e978d4559b9939a0087ab4955a84fdff357e
Noble Nwachukwu <ncnwachukwu@mix.wvu.edu>
2022-10-04 16:48:59 -0500

    formatted css and also added media queries

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # punk-api
+This application is fairly simple to run. Fork or clone the code over to your github, open in your local browser and you are good to go.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const urlApi = `https://api.punkapi.com/v2/beers?page=2&per_page=20`;
+const urlApi = `https://api.punkapi.com/v2/beers?page=1&per_page=20`;
 
 window.onload = async function (){
     const response = await fetch (urlApi);
@@ -6,7 +6,7 @@ window.onload = async function (){
     console.log(data)
 
     let data1 = ''
-
+    // Mapping over beer api then adding each beer to variable.
     data.map((beer) =>{
         data1 += `<div class="card">
         <img src=${beer.image_url} alt="">
@@ -26,14 +26,14 @@ window.onload = async function (){
         </div>
         </div>`
     })
-
+        // Grabbing the element by ID that I want to insert the data in
         document.getElementById("cards").innerHTML = data1;
 }
 
 
 
 document.querySelector('button').addEventListener('click',getBeer)
-
+// Creating button that collects the data and filters through api
     async function getBeer(){
         let flavor = document.getElementById('searchtxt').value
         const response = await fetch (`https://api.punkapi.com/v2/beers?beer_name=${flavor}`);
@@ -68,14 +68,14 @@ document.querySelector('button').addEventListener('click',getBeer)
    document.getElementById('btn2').addEventListener('click',getMore)
 
    async function getMore() {
-    const response = await fetch (`https://api.punkapi.com/v2/beers?page=2&per_page=40`);
+    const response = await fetch (`https://api.punkapi.com/v2/beers?page=1&per_page=40`);
     const data = await response.json();
     console.log(data)
 
     let data3 = '';
 
     data.map((beer) => {
-        data2 += ` <div class="card">
+        data3 += ` <div class="card">
       <img src=${beer.image_url} alt="">
       <div class="text">
       <h1 class="beer-name">${beer.name}</h1>
