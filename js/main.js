04b1b4d9359f12169e5f1e06bcd8dc12c73c336b
Noble Nwachukwu <ncnwachukwu@mix.wvu.edu>
2022-10-04 15:58:45 -0500

    added map function to map over api and also fetched api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 const urlApi = `https://api.punkapi.com/v2/beers?page=2&per_page=20`;
+
+window.onload = async function (){
+    const response = await fetch (urlApi);
+    const data = await response.json();
+    console.log(data)
+
+    let data1 = ''
+
+    data.map((beer) =>{
+        data += `<div class="card">
+        <img src=${beer.image_url} alt="">
+        <div class="text">
+        <h1>${beer.name}</h1>
+        <p>${beer.tagline}</p>
+        <p>${beer.description}</p>
+        <ul>
+        <h3>Pair it with</h3>
+        ${beer.food_pairing.map(e =>`<li> ${e} </li>`).join(' ')}
+        </ul>
+        <div class="specs">
+        <p>ABV: ${beer.abv} | </p>
+        <p>IBU: ${beer.ibu} | </p>
+        <p>PH: ${beer.ph}</p>
+        </div>
+        </div>
+        </div>`
+    })
+
+        document.getElementById("cards").innerHTML = data1;
+}
