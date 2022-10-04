57e5ced3db595d6d0c674bd5e2313524a8f85450
Noble Nwachukwu <ncnwachukwu@mix.wvu.edu>
2022-10-04 16:18:25 -0500

    added function to load 20 more beers

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ window.onload = async function (){
     let data1 = ''
 
     data.map((beer) =>{
-        data += `<div class="card">
+        data1 += `<div class="card">
         <img src=${beer.image_url} alt="">
         <div class="text">
         <h1>${beer.name}</h1>
@@ -29,3 +29,70 @@ window.onload = async function (){
 
         document.getElementById("cards").innerHTML = data1;
 }
+
+
+
+document.querySelector('button').addEventListener('click',getBeer)
+
+    async function getBeer(){
+        let flavor = document.getElementById('searchtxt').value
+        const response = await fetch (`https://api.punkapi.com/v2/beers?beer_name=${flavor}`);
+        const data = await response.json()
+        console.log(data)
+
+        let data2 = '';
+
+        data.map((beer) =>{
+            data2 += `<div class="card">
+            <img src=${beer.image_url} alt="">
+            <div class="text">
+            <h1 class="beer-name">${beer.name}</h1>
+            <p>${beer.tagline}</p>
+            <p>${beer.description}</p>
+            <ul> 
+            <h3>Pair it with</h3>
+             ${beer.food_pairing.map(e=>`<li> ${e} </li>`).join(' ')}
+            </ul>
+            <div class="specs">
+            <p>ABV: ${beer.abv} | </p>
+            <p>IBU: ${beer.ibu} | </p>
+            <p>PH: ${beer.ph}</p>
+            </div>
+            </div>
+          </div>`
+        })
+        document.getElementById("cards").innerHTML = data2
+    }
+
+
+   document.getElementById('btn2').addEventListener('click',getMore)
+
+   async function getMore() {
+    const response = await fetch (`https://api.punkapi.com/v2/beers?page=2&per_page=40`);
+    const data = await response.json();
+    console.log(data)
+
+    let data3 = '';
+
+    data.map((beer) => {
+        data2 += ` <div class="card">
+      <img src=${beer.image_url} alt="">
+      <div class="text">
+      <h1 class="beer-name">${beer.name}</h1>
+      <p>${beer.tagline}</p>
+      <p>${beer.description}</p>
+      <ul> 
+      <h3>Pair it with</h3>
+       ${beer.food_pairing.map(e=>`<li> ${e} </li>`).join(' ')}
+      </ul>
+      <div class="specs">
+      <p>ABV: ${beer.abv} | </p>
+      <p>IBU: ${beer.ibu} | </p>
+      <p>PH: ${beer.ph}</p>
+      </div>
+      </div>
+    </div>`
+      })
+      document.getElementById("cards").innerHTML = data3;
+    
+   }
